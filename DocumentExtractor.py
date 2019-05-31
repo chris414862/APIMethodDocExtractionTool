c2bfff2d7122904362357a1ce6ab2a373ae73974
chris414862 <chris414862@gmail.com>
2019-05-31 10:25:05 -0500

    just getting started2

--- DocumentExtractor.py
+++ DocumentExtractor.py
@@ -1,13 +1,19 @@
 from bs4 import BeautifulSoup
+import re
 
 with open("Account     Android Developers.htm", "rb") as html_doc:
     soup = BeautifulSoup(html_doc, 'html.parser')
 
 
 #print(soup.prettify())
-for thing in soup.find_all(class_="api-name", id="describeContents()"):
+
+for thing in soup.find_all("div", id="jd-content"):
     i=0
-    print(thing.parent.contents[7])
+    print("("+str(i)+") "+str(thing.prettify()))
+    #if re.search(r"Public methods")
+    #print("\t\t"+str(thing.parent.parent['id']))
+    print("\n\n\n")
+
     #for item in thing.parent.contents:
      #   print("("+str(i)+") "+str(item))
      #   i+=1
