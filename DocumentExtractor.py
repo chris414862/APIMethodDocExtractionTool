cbb7af393c2de0587c7a4aaf907f9c707c46082a
Chris Crabtree <chris414862@gmail.com>
2019-05-31 14:54:19 -0500

    finished parser

--- DocumentExtractor.py
+++ DocumentExtractor.py
@@ -1,19 +1,29 @@
 from bs4 import BeautifulSoup
 import re
 
-with open("Account     Android Developers.htm", "rb") as html_doc:
+with open("BackupManager     Android Developers.htm", "rb") as html_doc:
     soup = BeautifulSoup(html_doc, 'html.parser')
 
+i=0
 
-#print(soup.prettify())
-
-for thing in soup.find_all("div", id="jd-content"):
-    i=0
-    print("("+str(i)+") "+str(thing.prettify()))
-    #if re.search(r"Public methods")
-    #print("\t\t"+str(thing.parent.parent['id']))
-    print("\n\n\n")
-
-    #for item in thing.parent.contents:
-     #   print("("+str(i)+") "+str(item))
-     #   i+=1
+thing =soup.find(id="public-methods_1")
+thing =thing.find_next_sibling("div")
+while thing != None:
+    print("(" + str(i) + ") " + str(thing.find("h3").contents[0]))
+    textTag =thing.find("p")
+    for string in textTag.strings:
+        print(string, end="")
+    for tag in textTag.next_siblings:
+        if tag.name == "p":
+            for string in tag.strings:
+                print(string, end="")
+        if tag.name == "ul":
+            list = tag
+            for string in list.find("li").strings:
+                print(string, end="")
+            for listElement in list.find("li").next_siblings:
+                for string in listElement.strings:
+                    print(string, end="")
+    print()
+    thing = thing.find_next_sibling("div")
+    i += 1
